f2db24433a6cf2349b7ca31737999b2d02366870
fahrurrizqi <163149493+fahrurrizqi@users.noreply.github.com>
2024-06-04 19:58:53 +0700

    Add files via upload

---
+++ MODUL 8/unguided2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string kalimat;
+    cout << "Masukkan kalimat: ";
+    getline(cin, kalimat);
+
+    int jumlahVokal = 0;
+    char vokal[] = {'a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'};
+    int ukuranVokal = sizeof(vokal) / sizeof(vokal[0]);
+
+    
+    for (char huruf : kalimat)
+    {
+        for (int i = 0; i < ukuranVokal; i++)
+        {
+            if (huruf == vokal[i])
+            {
+                jumlahVokal++;
+                break;
+            }
+        }
+    }
+
+    cout << "Jumlah huruf vokal dalam kalimat: " << jumlahVokal << endl;
+
+    return 0;
+}
